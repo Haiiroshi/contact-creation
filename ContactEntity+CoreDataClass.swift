3e714716a24b79bb090d1b63a96a66015c0bf0eb
Melendez Hiroshi <jairoshi.7@gmail.com>
2021-02-24 17:56:18 -0400

    coredata part3

---
+++ ContactEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ContactEntity+CoreDataClass.swift
+//  Contact Creation
+//
+//  Created by Hiroshi Melendez on 2/24/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ContactEntity)
+public class ContactEntity: NSManagedObject {
+
+}
