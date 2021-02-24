59e89db8030708e09b45d9f5fcbea0849f9968be
Melendez Hiroshi <jairoshi.7@gmail.com>
2021-02-24 12:55:11 -0400

    cell design is ready

---
+++ Contact Creation/Contact.swift
@@ -0,0 +1,14 @@
+//
+//  Contact.swift
+//  Contact Creation
+//
+//  Created by Hiroshi Melendez on 2/24/21.
+//
+
+import Foundation
+
+struct Contact {
+    var name: String
+    var phoneNumber: String
+    var photoURL: URL? = URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png")
+}
---
+++ Contact Creation/ContactTableViewCell.swift
@@ -0,0 +1,89 @@
+//
+//  ContactTableViewCell.swift
+//  Contact Creation
+//
+//  Created by Hiroshi Melendez on 2/24/21.
+//
+
+import UIKit
+import Kingfisher
+
+fileprivate let photoImageViewSize: CGFloat = 100 - 12*2
+
+class ContactTableViewCell: UITableViewCell {
+    
+    static let identifier: String = ContactTableViewCell.description()
+    
+    
+    private let photoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = UIColor.lightGray
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 12
+        return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        return label
+    }()
+    
+    private let phoneLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.textColor = UIColor.darkGray
+        return label
+    }()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.accessoryType = .disclosureIndicator
+        contentView.addSubview(photoImageView)
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(phoneLabel)
+        setupLayout()
+    }
+    
+    func setupLayout(){
+        photoImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        photoImageView.heightAnchor.constraint(equalToConstant: photoImageViewSize).isActive = true
+        photoImageView.widthAnchor.constraint(equalToConstant: photoImageViewSize).isActive = true
+        photoImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 12).isActive = true
+        
+        nameLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -16).isActive = true
+        nameLabel.leftAnchor.constraint(equalTo: photoImageView.rightAnchor, constant: 12).isActive = true
+        nameLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -12).isActive = true
+        
+        phoneLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: 16).isActive = true
+        phoneLabel.leftAnchor.constraint(equalTo: photoImageView.rightAnchor, constant: 12).isActive = true
+        phoneLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -12).isActive = true
+    }
+    
+    func setContact(contact: Contact){
+        photoImageView.kf.setImage(with: contact.photoURL!)
+//        print(contact.photoURL)
+        nameLabel.text = contact.name
+        phoneLabel.text = contact.phoneNumber
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
